809747ba00150c6fef662b8c7f0d9f0bd13ae51c
Cooper-VR <cooperjbower@gmail.com>
2023-10-02 13:50:44 -0700

    idk

--- Assets/scripts/enemySpawner.cs
+++ Assets/scripts/enemySpawner.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Security.Cryptography;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class enemySpawner : MonoBehaviour
