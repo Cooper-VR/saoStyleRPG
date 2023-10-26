1e2580f710ba20c069d12bc0700a3775b4a57e33
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-10-26 13:53:03 -0700

    created second menu layer

---
+++ Assets/scripts/innerButtonHandler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SAOrpg.playerAPI.RPGsstuff.Menu
+{
+    public class innerButtonHandler : MonoBehaviour
+    {
+        #region variables
+
+        public int menuIndex;
+
+        private collisionChecker collisionChecker;
+        private Animator menuController;
+        private Image imageCom;
+
+        public Color alternateColor;
+        private Color startingColor;
+        private Color currentColor;
+
+        public Sprite innerAlternateSprite;
+        private Sprite innerStartingSprite;
+        private Image innerImageCom;
+        private Sprite innerCurrentSprite;
+
+        #endregion
+
+        #region start/update
+
+        private void Start()
+        {
+            menuController = transform.parent.parent.GetComponent<Animator>();
+            collisionChecker = GetComponent<collisionChecker>();
+
+            imageCom = GetComponent<Image>();
+            startingColor = imageCom.color;
+            currentColor = startingColor;
+
+            innerImageCom = transform.GetChild(1).GetComponent<Image>();
+            innerStartingSprite = innerImageCom.sprite;
+            innerCurrentSprite = innerStartingSprite;
+        }
+
+        private void Update()
+        {
+            imageCom.color = currentColor;
+            if (collisionChecker.currentlyTouching && (collisionChecker.collidedObject == "leftFinger" || collisionChecker.collidedObject == "rightFinger"))
+            {
+                currentColor = alternateColor;
+                innerCurrentSprite = innerAlternateSprite;
+            }
+            else
+            {
+                currentColor = startingColor;
+                innerCurrentSprite = innerStartingSprite;
+            }
+
+            if (collisionChecker.entered && (collisionChecker.collidedObject == "leftFinger" || collisionChecker.collidedObject == "rightFinger"))
+            {
+                buttonAction();
+            }
+        }
+
+        #endregion
+
+        private void buttonAction()
+        {
+            //menuController.SetInteger("firstLayer", menuIndex);
+        }
+    }
+}
