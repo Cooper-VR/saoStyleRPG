3b0bc565328af674918b824f192aa19011645bff
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-10-31 13:46:42 -0700

    base of the walking sounds done

--- Assets/scripts/walkSounds.cs
+++ Assets/scripts/walkSounds.cs
@@ -7,23 +7,14 @@ namespace SAOrpg.playerAPI.RPGsstuff.audio
 {
     public class walkSounds : MonoBehaviour
     {
-        #region vaariables
+        #region variables
         public soundArray[] soundArrays;
-
-        public bool airTime;
 
         private playerMovement movement;
         private dashing dashScript;
 
-        public Vector3 velocity;
-        public float speed;
-        public float offset = 0.5f;
+        public Vector2 velocity;
 
-        public Vector3 playerPosition;
-
-        public float dashWait;
-        public float betweenAudio;
-        public float audioCoolDown = 0f;
 
         public LayerMask[] soundMasks;
 
@@ -32,75 +23,40 @@ namespace SAOrpg.playerAPI.RPGsstuff.audio
         public AudioSource audioSource;
         public AudioSource landSound;
 
+        public float betweenSounds = 0.7f;
+        public float currentTime;
+        public float speedThreshold;
+
         #endregion
 
         private void Start()
         {
             movement = GetComponent<playerMovement>();
             dashScript = GetComponent<dashing>();
-            soundMasks = new LayerMask[soundArrays.Length];
         }
 
         void Update()
         {
+            
 
-            if (dashScript.isDashing == true)
-            {
-                dashWait = 0.2f;
-            }
-            if (dashScript.isDashing == false)
-            {
-                dashWait = dashWait - 1 * Time.deltaTime;
+            velocity.x = movement.velocity.x;
+            velocity.y = movement.velocity.z;
 
-            }
-            betweenAudio = betweenAudio + 1 * Time.deltaTime;
-            if (betweenAudio > 20)
+            if (velocity.magnitude > speedThreshold)
             {
-                audioCoolDown = 0;
-                betweenAudio = 0;
-            }
+                currentTime += 1 * Time.deltaTime;
 
-            if (movement.grounded == false)
-            {
-                audioSource.enabled = false;
-                airTime = true;
-
-            }
-            else if (movement.grounded == true)
-            {
-                if (audioCoolDown < -0.6)
+                if (currentTime  >= betweenSounds / velocity.magnitude)
                 {
-                    landSound.Play();
-                    betweenAudio = 0;
-                }
-                airTime = false;
-                audioSource.enabled = true;
-                audioCoolDown = 0;
-            }
-
-            if (airTime == true)
-            {
-                audioCoolDown = audioCoolDown - 1 * Time.deltaTime;
-            }
-
-
-
-            playerPosition = transform.position;
-
-            velocity = movement.GetVelocity();
-
-            speed = Mathf.Sqrt(Mathf.Pow(velocity.x, 2) + Mathf.Pow(velocity.y, 2) + Mathf.Pow(velocity.z, 2));
-            if (speed > 1.9f && speed < 3.1f && movement.grounded == true)
-            {
-                if (offset <= 0)
-                {
-                    randomNum = Random.Range(0, soundArrays[getCurrentLayer()].audio.Length);
-                    audioSource.clip = soundArrays[getCurrentLayer()].audio[randomNum];
+                    //play sound
+                    
+                    randomNum = Random.Range(0, soundArrays[0].audio.Length);
+                    audioSource.clip = soundArrays[0].audio[randomNum];
                     audioSource.Play();
-                    offset = 0.5f;
-                }
+                    
+                    currentTime = 0f;
 
-                offset = offset - 1 * Time.deltaTime;
+                }
             }
             
         }
