5f46d07c9326a72a1802e176568ca6a90571536d
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-11-30 14:33:37 -0700

    need to start making damge testing

--- Assets/scripts/enemyHandler.cs
+++ Assets/scripts/enemyHandler.cs
@@ -6,13 +6,17 @@ namespace SAOrpg.enemies
     public class enemyHandler : MonoBehaviour
     {
         public enemyScriptableObject enemyBehavior;
-
+        public int health;
         public NavMeshAgent agent = new NavMeshAgent();
 
+        private void Start()
+        {
+            health = enemyBehavior.health;
+        }
 
         public void DealDamage(int amount)
         {
-            enemyBehavior.health -= amount;
+            health -= amount;
         }
     }
 }
--- Assets/scripts/enemyScriptableObject.cs
+++ Assets/scripts/enemyScriptableObject.cs
@@ -11,9 +11,14 @@ namespace SAOrpg
         public int RunSpeed;
         public int WalkSpeed;
         public int health;
+        public int level;
+        //damge will be based on the holding weapon
+
+        public GameObject[] itemDrops;
 
         public enum generalBehavior
         {
+            testStill,
             Passisve,
             agressive,
             runaway,
