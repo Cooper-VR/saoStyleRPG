5ce579646ce117aaf8689aa6bdc9f66efd8af502
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-01-22 13:25:58 -0700

    buttom position handler done

--- Assets/scripts/SAOrpg/NPCs/buttonPosition.cs
+++ Assets/scripts/SAOrpg/NPCs/buttonPosition.cs
@@ -1,11 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SAOrpg.NPCs
 {
     public class buttonPosition : MonoBehaviour
     {
-        
+        public Transform cam;
+        public float distanceThreshold;
+        public GameObject mainButton;
+
+        private void Update()
+        {
+            if ((cam.position - transform.position).magnitude <= distanceThreshold)
+            {
+                mainButton.SetActive(true);
+                SetPosition();
+            }
+            else
+            {
+                mainButton.SetActive(false);
+            }
+        }
+        private void SetPosition()
+        {
+            Vector3 direction = (cam.transform.position - transform.position).normalized;
+            direction *= distanceThreshold * 0.35f;
+            mainButton.transform.position = direction + transform.position;
+
+            mainButton.transform.LookAt(cam);
+        }
     }
 }
