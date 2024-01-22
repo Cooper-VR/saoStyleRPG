6817b61ab5445267dc9af078e77877b11823ec65
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-01-22 13:37:21 -0700

    button handler done

---
+++ Assets/scripts/SAOrpg/NPCs/Menu/buttonHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace SAOrpg.NPCs.Menu
+{
+    public class buttonHandler : MonoBehaviour
+    {
+        collisionChecker checker;
+        bool opened;
+
+        private void Start()
+        {
+            checker = GetComponent<collisionChecker>();
+        }
+
+        private void Update()
+        {
+            if ((checker.entered && checker.collidedObject.Contains("finger")) || checker.TestEnter)
+            {
+                //do action
+                if (opened)
+                {
+                    opened = false;
+                }
+                if (!opened)
+                {
+                    opened = true;                
+                }
+
+                transform.GetChild(0).gameObject.SetActive(opened);
+            }
+        }
+    }
+}
